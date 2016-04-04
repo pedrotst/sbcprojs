b18d525b579d6dc98dfa74aabb31e113f68c507d
pedroabreu <x01205375163@E-098567.tcu.gov.br>
2016-04-04 13:11:57 -0300

    First Commit

---
+++ src/base62.c
@@ -0,0 +1,5 @@
+#include "maze62.h"
+
+void test_fun(){
+    printf("Maze Module Included successufully");
+}
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "maze62.h"
+
+int main(){
+
+
+    printf("Working as aspected\n");
+    test_fun();
+
+    return 0;
+}

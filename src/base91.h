4dfce9f3538a71a1e5b45cc61030e27bc302b06f
Gess <gnsodre@gmail.com>
2016-04-07 22:03:16 -0300

    Commit inicial lendo arquivo b91

---
+++ src/base91.h
@@ -0,0 +1,16 @@
+#ifndef BASE64
+#define BASE64
+
+// Parâmetros:
+// *in : nome do arquivo de entrada.
+// *out : nome do arquivo de saída.
+void encode91(char *in, char *out);
+
+// Brief: decodifica um arquivo de entrada em base 64.
+//
+// Descrição:
+// Parâmetros:
+// *in : nome do arquivo de entrada.
+// *out : nome do arquivo de saída.
+void decode91(char *in, char *out);
+#endif

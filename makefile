2bd26fc4c3885ecd8ab995ce389e7b6a05449d3b
pedroabreu <abreu223@hotmail.com>
2016-04-08 21:22:48 -0300

    Codificando a primeira letra

--- makefile
+++ makefile
@@ -5,8 +5,8 @@ OBJ = ./obj/Debug/src
 
 all: main movemain
 
-main: base62.o base64.o moveobj
-	$(CC) $(CFLAGS) -o main $(SOURCE)/main.c $(OBJ)/base62.o $(OBJ)/base64.o
+main: base62.o base64.o base91.o moveobj
+	$(CC) $(CFLAGS) -o main $(SOURCE)/main.c $(OBJ)/base62.o $(OBJ)/base64.o $(OBJ)/base91.o
 
 base62.o: 
 	$(CC) $(CFLAGS) $(SOURCE)/base62.h $(SOURCE)/base62.c -c
@@ -14,6 +14,9 @@ base62.o:
 base64.o:
 	$(CC) $(CFLAGS) $(SOURCE)/base64.h $(SOURCE)/base64.c -c
 
+base91.o:
+	$(CC) $(CFLAGS) $(SOURCE)/base91.h $(SOURCE)/base91.c -c
+	
 moveobj:
 	mv ./*.o ./obj/Debug/src/
 
--- src/base91.c
+++ src/base91.c
@@ -1,14 +1,44 @@
 #include "base91.h"
 #include <stdio.h>
 #include <string.h>
+
+int get_bit(char c, int bit_n){
+    return (c >> bit_n) & 1;
+}
+
+static char *ENCODING_TABLE= 
+  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', '$',
+  '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=',
+  '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', '"'};
+
+static int *DECODE_TABLE = 
+  {91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 62, 90, 63, 64, 65, 66, 91, 67, 68, 69, 70, 71, 91, 72, 73,
+  52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 74, 75, 76, 77, 78, 79,
+  80,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+  15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 81, 91, 82, 83, 84,
+  85, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+  41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 86, 87, 88, 89, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
+  91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91};
+
 void encode91(char *in, char *out) {
     FILE *fp_in = NULL, *fp_out = NULL;
-    char c = !EOF, inBytes[3], outBytes[4];
-    int d[13];
+    char c1, c2;
+    int b13_bits, remainder;
     int i = 0;
 
     if ((fp_in = fopen(in, "rb")) == NULL) {
-        printf("%s", in);
         printf("\nErro ao abrir o arquivo de entrada.\n\n");
         return;
     }
@@ -17,9 +47,35 @@ void encode91(char *in, char *out) {
         printf("\nErro ao abrir o arquivo de saída.\n\n");
         return;
     }
-    fgets(d, 13, fp_in);
-    for(i =0; i < 14; i++)
-        printf("%d", d[i]);
+
+
+    c1 = fgetc(fp_in);
+    c2 = fgetc(fp_in);
+    //printf("%d\n", c1 << 8);
+    //concatena c1 e os cinco primeiros bits de c2
+    b13_bits = c1 << 5 | (c2 >> 3);
+    
+    //pega os 3 ultimos bits de c2
+    remainder = c2 & 0x7;
+    printf("%d\n", b13_bits);
+    printf("%d\n", remainder);
+    c1 = b13_bits >> 8;
+    //pega os 5 ultimos bits de b13 shifta 3  e junta com remainder
+    c2 = ((b13_bits & 0x1f) << 3) | remainder;
+    printf("%d\n", c1); 
+    printf("%d\n", c2); 
+
+/*
+    printf("%d\n", c >> 3 & 1);
+    for(i = 0; i < 8; i++){
+        c2 = get_bit(c, i); 
+        //fprintf("%d", c2);
+        printf("%d", c2);
+    }
+*/
+    printf("\n");
+    fclose(fp_in);
+    fclose(fp_out);
 }
 
 void decode91(char *in, char *out){

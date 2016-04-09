931bffa3542f17976f6a74f7a7be8c7828313c91
Felipe Albuquerque <falbuquerque@felipes-macbook-pro-2.local>
2016-04-09 13:09:02 -0300

    Aumentei o tamanho dos arrays do base85. O programa escrevia depois do fim do array.

--- src/base85.c
+++ src/base85.c
@@ -7,7 +7,7 @@ void encode85(char *in, char *out)
 {
 	
     FILE *fp_in = NULL, *fp_out = NULL;
-    char c = !EOF, inBytes[3];
+    char c = !EOF, inBytes[4];
     int i = 0 , aux;
     
     if ((fp_in = fopen(in, "rb")) == NULL) {
@@ -55,7 +55,7 @@ void decode85(char *in, char *out)
 {
 	
 	FILE *fp_in = NULL, *fp_out = NULL;
-    char c = !EOF, inBytes[4];
+    char c = !EOF, inBytes[5];
     int i = 0 , j=0 , aux;
     
     if ((fp_in = fopen(in, "rb")) == NULL) {

41b3779883b813adc847d5df003a95f051adc1fb
lipbarreto <felipe.barretoo@hotmail.com>
2016-04-10 19:49:12 -0300

    Update base85.c

--- src/base85.c
+++ src/base85.c
@@ -20,13 +20,15 @@
  */
 #include "base85.h"
 
+
 void encode85(char *in, char *out) 
 {
 	
     FILE *fp_in = NULL, *fp_out = NULL;
     char  outBytes[4];
-    int i = 0 ,  contAux = 0;
-    unsigned int aux, c = !EOF, inBytes[3] ;
+    int i = 0 ,  contAux = 0 , lim = 0, count = 0;
+    unsigned int  c = !EOF ;
+    uint32_t aux = 0;
     
     if ((fp_in = fopen(in, "rb")) == NULL) {
         printf("\nErro ao abrir o arquivo de entrada.\n\n");
@@ -38,94 +40,114 @@ void encode85(char *in, char *out)
         exit(0);
     }
     
-    
-    
     while (feof(fp_in) == 0) {
     	
-    	for (i = 0; i < 4; i++)
-        	inBytes[i] = 0;
-        
         for (i = 0; i < 4; i++) {
             if ((c = getc(fp_in), feof(fp_in)) == 0){
-				inBytes[i] = c;
+				aux |= ((unsigned int)c) << ((3 - count++) * 8);
 				}
 
             else
                 break;
         }
         
+		if (aux == 0 && count == 4 ){ 
+			putc (122	, fp_out); 
+			contAux++; 
+			if (contAux % 80 == 0) 
+				putc(10 , fp_out); 
+			
+			aux = 0;
+    		count = 0;
+			continue;
+			}
 		
-	    aux = inBytes[0]*256*256*256 + inBytes[1]*256*256 + inBytes[2]*256 + inBytes[3];
-
-		
-		if (aux == 0 ) 		{ putc (122							, fp_out); contAux++; continue;} 
 		else{
-			if (inBytes[0] > 0) { putc (aux/(85*85*85*85) % 85 + 33	|0, fp_out);contAux++; }
-        	if (inBytes[0] > 0) { putc (aux/(85*85*85) % 85 + 33	|0, fp_out);contAux++; }
-        	if (inBytes[1] > 0) { putc (aux/(85*85) % 85 + 33		|0, fp_out);contAux++; }
-    		if (inBytes[1] > 0) { putc (aux/(85) % 85 + 33			|0, fp_out);contAux++; }
-    		if (inBytes[2] > 0) { putc (aux%85 + 33					|0, fp_out);contAux++; }
-    		if (contAux % 80 == 0) putc ( 10 						|0, fp_out);
-    		if (feof(fp_in)) 	  fputs ("~>"						  , fp_out); 
-    		}
+			
+			for (i = 0; i < 5; i++) {
+				outBytes[i] = aux % 85 + '!';
+				aux /= 85;
+				}	
+		
+			lim = 4 - count;
+			for (i = 4; i >= lim; i--) {
+				putc ( outBytes[i] , fp_out);
+				contAux++;	
+				if (contAux % 80 == 0) 
+					putc(10 , fp_out);
+				}
+			
+
+   			}	
+    	
+		aux = 0;
+    	count = 0;
     	
 	}
+	
+	fputs ("~>" , fp_out);
 	
 	fclose(fp_in);
     fclose(fp_out);
 
 }
 
+int get_nextC(FILE *fp_in) {
+	
+	int c;
+	while (isspace(c = getc(fp_in)));
+	return c;
+	
+}
 
-void decode85(char *in, char *out)
+
+void decode85(char *in, char *out) 
 {
-
+	
 	FILE *fp_in = NULL, *fp_out = NULL;
-    char c = !EOF, inBytes[5];
+    char c = !EOF, inBytes[4];
     int i = 0 , j=0 , aux;
-
+    
     if ((fp_in = fopen(in, "rb")) == NULL) {
         printf("\nErro ao abrir o arquivo de entrada.\n\n");
         exit(0);
     }
-
+    
     if ((fp_out = fopen(out, "wb")) == NULL) {
         printf("\nErro ao abrir o arquivo de saída.\n\n");
         exit(0);
     }
-
+    
     while (feof(fp_in) == 0) {
-
+    	
 		for (i = 0; i < 5; i++)
         	inBytes[i] = 0;
-
+        
         for (i = 0; i < 5; i++) {
-            if ((c = getc(fp_in), feof(fp_in)) == 0){
+            if ((c = get_nextC(fp_in), feof(fp_in)) == 0){
 				inBytes[i] = c;
 			}
             else
                 break;
         }
-
-
-		for (j = 0; j < 5; j++) {
+		
+		
+		for (j = 0; j < 5; j++)
             if(( inBytes[j] < 33 && inBytes[j] > 117) || inBytes[j] != 122 )
 				inBytes[j] = inBytes[j] - 33;
-
-        }
-
+			
+		
 		aux = inBytes[0]*85*85*85*85 + inBytes[1]*85*85*85 + inBytes[2]*85*85 + inBytes[3]*85 + inBytes[4];
-
-
+		
 		if (inBytes[0] > 0) putc (aux/(256*256*256) % 256	, fp_out);
-        if (inBytes[1] > 0) putc (aux/(256*256) % 256		, fp_out);
-        if (inBytes[2] > 0) putc (aux/(256) % 256			, fp_out);
-    	if (inBytes[3] > 0) putc (aux % 256 				, fp_out);
-
+        if (inBytes[0] > 0) putc (aux/(256*256) % 256		, fp_out);
+        if (inBytes[1] > 0) putc (aux/(256) % 256			, fp_out);
+    	if (inBytes[1] > 0) putc (aux % 256 				, fp_out);
+    	
 	}
-
+	
 	fclose(fp_in);
     fclose(fp_out);
-
-
+	
+	
 }

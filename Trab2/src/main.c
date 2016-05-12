0f930a1dc85f7bd8730ede005bbed1e9cc5289e9
pedroabreu <abreu223@hotmail.com>
2016-05-11 22:59:32 -0300

    Comecando o paser

--- Trab2/src/main.c
+++ Trab2/src/main.c
@@ -19,4 +19,41 @@
  *  Copyright © 2016 UnB. All rights reserved.
  */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<inttypes.h>
+#include<math.h>
+#include<string.h>
 
+typedef uint32_t huge[8];
+
+void init_huge(huge h){
+    for(int i = 0 ; i < 8; i++){
+        h[i] = 0;
+    }
+}
+
+void str_to_huge(char* str, huge h){
+    int i = strlen(str) - 1;
+    int pow_index = 0;
+    while(i >= 0){
+        h[0] += (str[i] - '0') * pow(10, pow_index);
+        i--; pow_index++;
+    }
+
+}
+
+int main(int argc, char **argv){
+
+    huge h;
+    char num[79]; //maior numero de 255 bits tem tamanho 77, + sinal + \0
+    strcpy(num, argv[1]); 
+    init_huge(h);
+
+    printf("%d  %s\n", argc, argv[1]);
+    printf("%s\n", num);
+    str_to_huge(num, h);
+    printf("%" PRIu32 "\n", h[0]);
+    return 0;
+}

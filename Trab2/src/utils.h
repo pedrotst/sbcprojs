f7fa812d9b55f215e541239a9ebf51ac7a6e4a65
gessikete <gnsodre@gmail.com>
2016-05-14 17:16:23 -0300

    protótipos das funções principais necessárias

--- Trab2/src/utils.h
+++ Trab2/src/utils.h
@@ -29,18 +29,121 @@
 #include <math.h>
 #include <string.h>
 
- typedef uint32_t huge[8];
+ /*
+  * Tipo definito para manipulação de número com 256bits
+  */
+typedef uint32_t huge[8];
 
- void init_huge(huge h);
+void init_huge(huge h);
 
- int chtoi(char a);
+int chtoi(char a);
 
- char intochar(int i);
+char intochar(int i);
 
- int nilstring(char *str, int len);
+int nilstring(char *str, int len);
 
- void str_to_huge(char* num, huge h);
+void str_to_huge(char* num, huge h);
 
- void huge_to_str(huge h, char *num);
+void huge_to_str(huge h, char *num);
+
+ /*
+  * Brief       : Converte notação infixa para pós-fixa
+  *
+  * Descrição   : Converte notação infixa para pós-fixa
+  * Parâmetros  : char *infix: string com a operação infixa
+  * Retorno     : char *posfix: string com a operação pós-fix
+  */
+char * infix_to_postfix(char *infix);
+ // lembrar de incluir a prioridade dos parênteses
+
+ /*
+  * Brief       : Empilha um número
+  *
+  * Descrição   : Empilha um número tipo huge na pilha
+  *
+  * Parâmetros  : huge number: número de tamanho 256bits
+  *
+  * Retorno     : Nenhum
+  */
+void push(huge number);
+
+ /*
+  * Brief       : Desempilha um número
+  *
+  * Descrição   : Desempilha um número tipo huge da pilha
+  *
+  * Parâmetros  : Nenhum
+  *
+  * Retorno     : huge: número desempilhado tamanho 256bits
+  */
+huge pop(void);
+
+
+ /*
+  * Brief       : Executa a operação
+  *
+  * Descrição   : Executa a operação definida pelo operador passado 
+  *				  nos dois últimos números da pilha e empilha o resultado
+  *
+  * Parâmetros  : char operador: operador
+  *
+  * Retorno     : Nenhum
+  */
+void operacao(char operador);
+// essa função deve conter um switch comm o operador que direcionará para a operação
+
+ /*
+  * Brief       : Executa a operação de soma
+  *
+  * Descrição   : Executa a operação de soma nos dois números passados
+  *				  pela função chamadora
+  *
+  * Parâmetros  : huge dir: número de 256bits da direita
+  *				  huge esq: número de 256bits da esquerda
+  *
+  * Retorno     : huge result: número de 256bits resultante da operação de soma
+  */
+huge soma(huge dir, huge esq);
+
+ /*
+  * Brief       : Executa a operação de diferença
+  *
+  * Descrição   : Executa a operação de diferença nos dois números passados
+  *				  pela função chamadora
+  *
+  * Parâmetros  : huge dir: número de 256bits da direita
+  *				  huge esq: número de 256bits da esquerda
+  *
+  * Retorno     : huge result: número de 256bits resultante da operação de diferença
+  */
+huge subtrai(huge dir, huge esq);
+
+ /*
+  * Brief       : Executa a operação de multiplicação
+  *
+  * Descrição   : Executa a operação de multiplicação nos dois números passados
+  *				  pela função chamadora
+  *
+  * Parâmetros  : huge dir: número de 256bits da direita
+  *				  huge esq: número de 256bits da esquerda
+  *
+  * Retorno     : huge result: número de 256bits resultante da operação de multiplicação
+  */
+huge multiplica(huge dir, huge esq);
+
+ /*
+  * Brief       : Executa a operação de divisao
+  *
+  * Descrição   : Executa a operação de divisao nos dois números passados
+  *				  pela função chamadora
+  *
+  * Parâmetros  : huge dir: número de 256bits da direita
+  *				  huge esq: número de 256bits da esquerda
+  *
+  * Retorno     : huge result: número de 256bits resultante da operação de divisao
+  */
+huge divide(huge dir, huge esq);
+
+
 
  #endif /* _UTILS_H_ */
